26670e81f2cd75c08ee0637bf31986516ef30f5c
JunkyByte <adriano.donninelli@hotmail.it>
2020-01-10 02:52:09 +0100

    wip graph with adjacency matrix, adapted decorator to allow no parameter / multiple parameter

--- usage.py
+++ usage.py
@@ -4,6 +4,10 @@ from vispipe import vispipe
 def custom_block_test_identity(input1):
     yield input1
 
+@vispipe.block(output_names=['y1', 'y2'])
+def custom_block_test_identity_2_out(input1, input2):
+    yield (input1, input2)
+
 @vispipe.block
 def custom_block_test_plus1(input1):
     yield input1 + 1
@@ -12,5 +16,11 @@ def custom_block_test_plus1(input1):
 def custom_block_test_addition(input1, input2):
     yield input1 + input2
 
-vispipe.pipeline.add_pipeline(vispipe.pipeline._blocks['custom_block_test_addition'])
-vispipe.pipeline.build()
+vispipe.pipeline.add_node(vispipe.pipeline._blocks['custom_block_test_addition'])
+vispipe.pipeline.add_node(vispipe.pipeline._blocks['custom_block_test_addition'])
+vispipe.pipeline.add_node(vispipe.pipeline._blocks['custom_block_test_addition'])
+vispipe.pipeline.add_node(vispipe.pipeline._blocks['custom_block_test_plus1'])
+vispipe.pipeline.add_node(vispipe.pipeline._blocks['custom_block_test_identity_2_out'])
+#vispipe.pipeline.build()
+
+print(0)
--- vispipe/vispipe.py
+++ vispipe/vispipe.py
@@ -1,21 +1,28 @@
+import numpy as np
+from functools import partial
 from inspect import signature, isgeneratorfunction
 from typing import List, Callable
 from threading import Thread, Event
 from queue import Queue
 
+MAXSIZE = 100
+
 class Pipeline:
     def __init__(self):
         self._blocks = {}
-        self.pipeline = []
+        self.pipeline = PipelineGraph()
         self.runner = PipelineRunner()
 
-    def add_block(self, f : Callable, max_queue : int, output_names=None) -> None:
+    def register_block(self, f : Callable, max_queue : int, output_names=None) -> None:
         block = Block(f, signature(f).parameters, max_queue, output_names)
-        assert block.name not in self._blocks.keys()
+        assert block.name not in self._blocks.keys(), 'The name %s is already registered as a pipeline block' % block.name
         self._blocks[block.name] = block
 
-    def add_pipeline(self, block):
-        self.pipeline.append(block)
+    def add_node(self, block):
+        self.pipeline.add_node(block)
+
+    def add_conn(self):
+        raise NotImplementedError
 
     def build(self) -> None:
         # ask runner to build the pipeline
@@ -31,7 +38,6 @@ class PipelineRunner:
     def build_pipeline(self, pipeline):
         # Collect each block arguments, create connections between each other using separated threads
         # use queues to pass data between the threads.
-        # The first block of the pipeline must be connected solely to an input generator
         self.built = True
         for block in pipeline:
             in_q = Queue(10)
@@ -51,6 +57,37 @@ class PipelineRunner:
             print(i)
             time.sleep(3)
 
+class PipelineGraph:
+    def __init__(self):
+        self.matrix = np.empty((MAXSIZE, MAXSIZE), dtype=object)  # Adjacency matrix
+        self.inputs = np.zeros((MAXSIZE,), dtype=np.int)
+
+        self.ids = {}  # Map from hash to assigned ids
+        self.free_idx = set()
+        self.free_idx.update([i for i in range(MAXSIZE)])
+
+    def get_hash(self, block):
+        return hash(block)
+
+    def register_node(self, block, id):
+        hash_block = self.get_hash(block)
+        self.ids[hash_block] = id
+
+    def add_node(self, block):
+        id = self.free_idx.pop()
+        self.register_node(block, id)  # Register block in the matrix
+        num_inputs = block.num_inputs()
+        num_outputs = block.num_output()
+        self.inputs[id] = num_inputs  # Store inputs
+        out = [0 for _ in range(num_outputs)]
+        for i in range(MAXSIZE): self.matrix[id, i] = out  # and outputs
+
+    def remove_node(self):
+        raise NotImplementedError
+
+    def add_connection(self):
+        raise NotImplementedError
+
 class Block:
     def __init__(self, f: Callable, input_args: List[str], max_queue: int, output_names: List[str]):
         self.f = f
@@ -59,6 +96,12 @@ class Block:
         self.max_queue = max_queue
         self.output_names = output_names if output_names != None else ['y']
 
+    def num_inputs(self):
+        return len(self.input_args)
+
+    def num_output(self):
+        return len(self.output_names)
+
     def __iter__(self):
         yield 'f', self.f
         yield 'name', self.name
@@ -66,7 +109,7 @@ class Block:
         yield 'max_queue', self.max_queue
         yield 'output_names', self.output_names
 
-def block(f : callable, max_queue=2, output_names=None, tag='None'):
+def block(f=None, max_queue=2, output_names=None, tag='None'):
     """
     Decorator function to tag custom blocks to be added to pipeline
     :param f: The function to tag (as a decorator it will be automatically passed)
@@ -75,12 +118,15 @@ def block(f : callable, max_queue=2, output_names=None, tag='None'):
     :param tag: Tag to organize decorated blocks
     :return:
     """
+    if f is None:  # Correctly manage decorator duality
+        return partial(block, max_queue=max_queue, output_names=output_names, tag=tag)
+
     if not isgeneratorfunction(f):
         raise TypeError('The function you tagged is not a generator')
     if signature(f).parameters and list(signature(f).parameters.keys())[0] == 'self':
         raise TypeError('The function you passed is a class method, we only support functions right now')
 
-    pipeline.add_block(f, max_queue, output_names)
+    pipeline.register_block(f, max_queue, output_names)
     return f
 
 def run_block(f, in_q, out_q, *args, **kwargs):
