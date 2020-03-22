8a5494dff261821ac75c68197712df26826a4d7e
JunkyByte <adriano.donninelli@hotmail.it>
2020-03-22 16:18:18 +0100

    Plot vis added enable / disable axis (with disabled axis not using all space, it's a bug)

--- vispipe/ops/vis.py
+++ vispipe/ops/vis.py
@@ -21,23 +21,38 @@ def vis_shape(input1):
 
 @block(tag='vis', max_queue=1, data_type='plot')
 class plt_plot:
-    def __init__(self):
+    def __init__(self, show_axis: bool = True):
+        self.show_axis = show_axis
         self.fig = plt.Figure(figsize=[1.2, 1.2])
         self.ax = self.fig.gca()
-        #self.ax.axis('off')
-        self.ax.margins(0)
-        self.ax.tick_params(axis='x', pad=2, labelsize=6, direction='in', length=2)
-        self.ax.tick_params(axis='y', pad=1, labelsize=6, direction='in', length=2, rotation=90)
-        self.fig.tight_layout(pad=0.1)
-        self.pl, = self.ax.plot([], [])
+
+        if self.show_axis:
+            self.ax.tick_params(axis='x', pad=2, labelsize=6, direction='in', length=2)
+            self.ax.tick_params(axis='y', pad=1, labelsize=6, direction='in', length=2, rotation=90)
+            self.fig.tight_layout(pad=0.1)
+            self.ax.margins(0)
+            self.pl, = self.ax.plot([], [])
+            self.fig.canvas.draw()
+            self.bg = self.fig.canvas.copy_from_bbox(self.ax.bbox)
+        else:
+            self.ax.axis('off')
+            self.fig.tight_layout(pad=0)
+            self.ax.margins(0)
 
     def run(self, x, y):
-        #self.ax.cla()
-        self.pl.set_xdata(x)
-        self.pl.set_ydata(y)
-        self.ax.relim()
-        self.ax.autoscale_view()
+        if self.show_axis:
+            self.pl.set_data(x, y)
+            self.ax.relim()
+            self.ax.autoscale_view()
+            self.fig.canvas.restore_region(self.bg)
+            self.ax.draw_artist(self.pl)
+            self.fig.canvas.blit(self.ax.bbox)
+        else:
+            self.ax.cla()
+            self.ax.plot(x, y)
+            self.ax.axis('off')
+            self.ax.margins(0)
+            self.fig.canvas.draw()
 
-        self.fig.canvas.draw()
         self.fig.canvas.flush_events()
         yield self.fig
